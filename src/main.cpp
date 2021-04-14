ee528fcd2c85afa834525d2b5b36b90b3b65a862
Ygenks <eugentoo@gmail.com>
2021-04-14 13:59:23 +0300

    Add SIM800 library

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,56 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include <Arduino.h>
+#include <ThreadedGSM.h>
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+#define SerialDebug Serial
+#define SerialModem Serial
+#define THREADEDGSM_DEBUG SerialDebug
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+ThreadedGSM SIM900(SerialModem);
+
+void clock(ThreadedGSM &modem, ThreadedGSM::NetworkTime &Time) {
+  SerialDebug.print("Modem Time: ");
+  SerialDebug.print(Time.day);
+  SerialDebug.print("/");
+  SerialDebug.print(Time.month);
+  SerialDebug.print("/");
+  SerialDebug.print(Time.year);
+  SerialDebug.print(" ");
+  SerialDebug.print(Time.hour);
+  SerialDebug.print(":");
+  SerialDebug.print(Time.minute);
+  SerialDebug.print(":");
+  SerialDebug.println(Time.second);
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+void signal(ThreadedGSM &modem, ThreadedGSM::SignalLevel &Signal) {
+  SerialDebug.print("Modem signal: Dbm:");
+  SerialDebug.print(Signal.Dbm);
+  SerialDebug.print(" value: ");
+  SerialDebug.println(Signal.Value);
 }
+
+void sms(ThreadedGSM &modem, String &Msg) {
+  SerialDebug.print("Received SMS: ");
+  SerialDebug.println(Msg);
+}
+
+void startup(ThreadedGSM &modem) { SerialDebug.print("Ready"); }
+
+void setup() {
+  SerialDebug.begin(115200);
+  SerialModem.begin(115200);
+  SerialDebug.println("STARTUP\r\n");
+
+  SIM900.begin();
+  SIM900.setInterval(ThreadedGSM::INTERVAL_CLOCK, 60000);
+  SIM900.setInterval(ThreadedGSM::INTERVAL_SIGNAL, 60000);
+  SIM900.setInterval(ThreadedGSM::INTERVAL_INBOX, 30000);
+  SIM900.setHandlers({.signal = signal,
+                      .clock = clock,
+                      .incoming = sms,
+                      .ready = startup,
+                      .outgoing = NULL,
+                      .power = NULL});
+}
+
+void loop() { SIM900.loop(); }
